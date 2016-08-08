00c090f7577a11c28c3099ffc5d855c0dc6d8707
David Acuna <David_acuna@abtassoc.com>
2016-08-08 15:00:30 -0400

    addding abt pages

---
+++ AbtFramework/AbtPages.cs
@@ -0,0 +1,14 @@
+﻿namespace AbtFramework
+{
+    public enum AbtPages
+    {
+        Oracle,
+        Abt_Talent_Learning_and_Support,
+        AbtKnowledge,
+        AbtTravel,
+        ISMS,
+        Outlook,
+        RepCapPlanner,
+        ReputationalCapitalDB
+    }
+}
